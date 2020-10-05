e3ef28404569638e3f6a87f903be258f056a41f1
Paco17 <326franciscorv@gmail.com>
2020-10-04 19:55:35 -0500

    Get Countries Done

--- Rest Countries/app.js
+++ Rest Countries/app.js
@@ -1,10 +1,50 @@
 //TODOS's:
-//Theme Toggle
 //Search
 //Filter
 //Modal
 //API call
 
+
+const countriesEl = document.getElementById('countries');
+const toggleBtn = document.getElementById('toggle');
+
+getCountries();
+
 async function getCountries() {
-    const
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const countries = await res.json();
+
+    showCountries(countries);
+    console.log(countries);
 }
+
+function showCountries(countries) {
+    countries.forEach(country => {
+        const countryEl = document.createElement('div');
+        countryEl.classList.add('card');
+        countryEl.innerHTML = `
+        <div>
+            <img src="${country.flag}" alt="Germany"/>
+        </div>
+        <div class="card-body">
+            <h2>${country.name}</h2>
+            <p>
+                <strong>Population: </strong>
+                ${country.population}
+            </p>
+            <p>
+                <strong>Region: </strong>
+                ${country.region}
+            </p>
+            <p>
+                <strong>Capital: </strong>
+                ${country.capital}
+            </p>
+        </div>`
+        countriesEl.appendChild(countryEl);
+    });
+}
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+});
